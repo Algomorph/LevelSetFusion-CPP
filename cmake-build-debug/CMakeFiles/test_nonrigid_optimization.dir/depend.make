c338acffa2dd003f7714d0caa05a904b5b7f2a54
Gregory Kramida <algomorph@gmail.com>
2018-12-05 12:37:11 -0500

    clearing out cmake-build-debug build directory

--- cmake-build-debug/CMakeFiles/test_nonrigid_optimization.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_nonrigid_optimization.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp
-# Build directory: /home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(nonrigid_optimization "/home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp/cmake-build-debug/test_nonrigid_optimization")
-add_test(math "/home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp/cmake-build-debug/test_math")
