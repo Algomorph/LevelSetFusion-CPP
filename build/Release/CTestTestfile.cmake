7c2001cb325d18272de6ebb1ba1aa2da419b0aea
Gregory Kramida <algomorph@gmail.com>
2018-12-05 11:20:46 -0500

    initial commit from LevelSetFusion2DExperiments repo

---
+++ build/Release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp
+# Build directory: /home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp/build/Release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(nonrigid_optimization "/home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp/build/Release/test_nonrigid_optimization")
+add_test(math "/home/algomorph/Workbench/LevelSetFusion2DExperiments/cpp/build/Release/test_math")
---
+++ cmake-build-debug/CMakeFiles/test_nonrigid_optimization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_nonrigid_optimization.dir/tests/test_nonrigid_optimization.cpp.o"
+  "test_nonrigid_optimization.pdb"
+  "test_nonrigid_optimization"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_nonrigid_optimization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
