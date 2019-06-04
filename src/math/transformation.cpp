353e2baffacdb6916057e3dbab867540ba5cb307
fshanAtUMD <fshan@terpmail.umd.edu>
2019-06-03 20:51:54 -0400

    transformation template.

--- src/math/transformation.cpp
+++ src/math/transformation.cpp
@@ -1,4 +1,14 @@
-//
-// Created by fei on 6/3/19.
-//
+//  ================================================================
+//  Created by Fei Shan on 03/05/19.
+//  Converting transformation vector to matrix in 2D and 3D homogeneous coordinates.
+//  ================================================================
+
+#include "transformation.tpp"
+
+namespace math {
+
+template eig::Matrix3f transformation_vector_to_matrix<float>(const eig::Vector3f& twist);
+template eig::Matrix4f transformation_vector_to_matrix<float>(const eig::Matrix<float, 6, 1>& twist);
+
+}
 
